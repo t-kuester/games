afb616103c3593f66e2de9dc60695c485d1bd4c5
t-kuester <tobias.kuester@gmx.net>
2019-07-13 15:01:25 +0200

    determine winning moves and non-losing moves

--- Ultimate TicTacToe/uttt.py
+++ Ultimate TicTacToe/uttt.py
@@ -1,17 +1,15 @@
 # TODO
-# determine losing moves (other player can win with next move)
 # proper monte carlo tree search
 # fast method for board copying (maybe try flat board again?)
 
 import sys, functools, random, time, copy, itertools
 
-random.seed(0)
+
 debug = functools.partial(print, file=sys.stderr)
 rows = range(3) # yes, this does makes a difference!
 
 SYMBOL = [".", "X", "#", "O"]
 NONE, OWN, OPP, DRAW = 0, 1, -1, 2
-TIMEOUT = 0.5
 
 def get_moves(board, last):
 	""" get valid moves given board and last move
@@ -22,18 +20,23 @@ def get_moves(board, last):
 	return [(3*R+r, 3*C+c) for (R,C) in playable_fields
 			for r in rows for c in rows if board[R][C][r][c] == NONE]
 
+def winning_move(board, moves, player):
+	""" get move that leads to immediate victory, if any, or None
+	"""
+	return next((m for m in moves if winning(apply_move(board, m, player), player)), None)
+
+def non_losing_moves(board, moves, player):
+	""" get moves that do not lead to victory of other player in next turn
+	"""
+	return [m for m, nb in ((m, apply_move(board, m, player)) for m in moves)
+	        if not winning_move(nb, get_moves(nb, m), -player)]
+
 def winning_draw(board, player):
 	""" In case of no line, player with more smaller boards wins
 	"""
 	own = sum(1 for line in board for c in line if c == +player)
 	opp = sum(1 for line in board for c in line if c == -player)
 	return player if (own > opp) else (-player if opp > own else 0)
-
-def losing_move(board, move, player):
-	# just for testing, copy board and apply the move (should not be necessary)
-	# check if the move is a winning move
-	# if not, get moves after that, check if any of those is a winning move
-	pass
 
 def winning(board, player):
 	""" check whether player has won the board, works for sub- or for meta board
@@ -60,11 +63,10 @@ def apply_move(board, move, player):
 		
 	return board
 	
-def random_play(move, player):
+def random_play(board, move, player):
 	""" perform random moves until the game is over,
 	return winning player and number of moves
 	"""
-	board = partial_copy(grid, move) # redundant, but keep in for now
 	board = apply_move(board, move, player)
 	for i in itertools.count():
 		if winning(board, player):
@@ -72,28 +74,43 @@ def random_play(move, player):
 		moves = get_moves(board, move)
 		if not moves:
 			return winning_draw(board, player), i
-		player = - player
+		player = -player
 		board = apply_move(board, random.choice(moves), player)
 
-def best_move(player):
+def best_move_random_plays(board, moves, player):
 	""" perform random plays for random moves until time is up,
 	select move with best win/loss ratio
 	"""
-	moves = {move: 0 for move in valid_actions}
-	start = time.time()
+	scores = {move: 0 for move in moves}
 	
+	start = time.time()
 	total = 0
 	for i in itertools.count():
 		if time.time() - start > TIMEOUT:
 			break
-		move = random.choice(valid_actions)
-		r, c = random_play(move, player)
-		moves[move] += player * r
+		move = random.choice(moves)
+		r, c = random_play(board, move, player)
+		scores[move] += player * r
 		total += c
 	
 	debug(i, total, moves)
-	return max((moves[m], m) for m in moves)
+	return max((scores[m], m) for m in moves)
 
+def best_move(board, moves, player):
+	""" return winning move, or best non-losing move, if any
+	"""
+	win_move = winning_move(board, moves, player)
+	if win_move:
+		debug("WINNING MOVE")
+		return 999, win_move
+	else:
+		non_lose = non_losing_moves(board, moves, player)
+		if non_lose:
+			return best_move_random_plays(board, non_lose, player)
+		else:
+			debug("only losing moves...")
+			return best_move_random_plays(board, moves, player)
+	
 def partial_copy(board, move):
 	""" create partial copy of only the affected parts of the board
 	about 10% slower... maybe with flat lists? should be ~5x faster
@@ -103,8 +120,7 @@ def partial_copy(board, move):
 	board[R][C] = list(map(list, board[R][C]))
 	return board
 	
-
-def show_grid():
+def show_grid(board):
 	""" draw a nice representation of the grid; mainly to check apply_move
 	"""
 	for r in range(9):
@@ -114,38 +130,56 @@ def show_grid():
 		for c in range(9):
 			if c in (3, 6):
 				line += "|"
-			g = grid[r//3][c//3]
+			g = board[r//3][c//3]
 			x = g if type(g) == int else g[r%3][c%3]
 			line += SYMBOL[x]
 		debug(' '.join(line))
 
 
 # INITIALIZATION
-grid = [[[[NONE for _ in range(3)] for _ in range(3)]
-				for _ in range(3)] for _ in range(3)]
-valid_actions = []
 
 def play_game():
-	player, move = OPP, (-1, -1)
-	while True:
-		valid_actions[:] = get_moves(grid, move)
-		if not valid_actions:
-			return 0
-		
-		if 1 or player == OWN:
-			score, move = best_move(player)
-		else:
-			score, move = 0, random.choice(valid_actions)
-		
-		debug("move: {}, score: {}".format(move, score))
-		board = apply_move(grid, move, player)
+	board = [[[[NONE for _ in range(3)] for _ in range(3)]
+					 for _ in range(3)] for _ in range(3)]
+	player, move = random.choice([OWN, OPP]), (-1, -1)
+	for i in itertools.count():
+		moves = get_moves(board, move)
+		if not moves:
+			r = winning_draw(board, player)
+			if r:
+				show_grid(board)
+			return r
 
-		show_grid()
-		print(*move)
+		if player == OPP:
+			score, move = best_move(board, moves, player)
+		else:
+			score, move = best_move_random_plays(board, moves, player)
 		
-		if winning(grid, player):
+		debug("{} move: {}, score: {}".format(i, move, score))
+		board = apply_move(board, move, player)
+
+		show_grid(board)
+		
+		if winning(board, player):
 			return player
 		player = -player
 
-r = play_game()
-print("Winner:", SYMBOL[r])
+seed = random.randrange(1000000)
+random.seed(seed)
+TIMEOUT = 0.5
+results = {+1: 0, 0: 0, -1: 0}
+for i in range(100):
+	r = play_game()
+	results[r] += 1
+	print(i, r, results)
+	break
+print(results)
+print(seed)
+
+# WHY ARE RESULTS STILL BIASED TOWARDS PLAYER +1 ?
+# {0: 17, 1: 52, -1: 31} best move random play vs. best move random play
+# {0: 6, 1: 54, -1: 40} BMRP vs BMRP + winning-draw
+# {0: 3, 1: 42, -1: 55} BMRP vs. BM + winning move
+# {0: 14, 1: 55, -1: 31} BM + winning move vs BMRP
+# {0: 8, 1: 66, -1: 26} BM + winning + non-losing vs BMRP
+# {0: 5, 1: 30, -1: 65} BMRP vs BM + winning + non-losing
