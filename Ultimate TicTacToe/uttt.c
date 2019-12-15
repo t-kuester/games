7bcdc368bd3b5e2b31a30ebe1ffe525cc95486c6
t-kuester <tobias.kuester@gmx.net>
2019-12-15 23:00:55 +0100

    implemented missing functions, first test looking good

--- Ultimate TicTacToe/uttt.c
+++ Ultimate TicTacToe/uttt.c
@@ -78,13 +78,16 @@ int winning_draw(int field[], int player) {
 }
 
 int winning(int field[], int player) {
+    int i, p = player;
+    // rows and columns
+    for (i = 0; i < 3; i++) {
+        if (field[  i] == p && field[  i+3] == p && field[  i+6] == p) return 1;
+        if (field[3*i] == p && field[3*i+1] == p && field[3*i+2] == p) return 1;
+    }
+    // diagonals
+    if (field[0] == p && field[4] == p && field[8] == p) return 1;
+    if (field[2] == p && field[4] == p && field[6] == p) return 1;
     return 0;
-	//~""" check whether player has won the board, works for sub- or for meta board
-	//~"""
-    //~return (any(all(board[r*3+c] == player for c in rows) for r in rows)
-     //~or any(all(board[r*3+c] == player for r in rows) for c in rows)
-     //~or all(board[i*3+   i]  == player for i in rows)
-     //~or all(board[i*3+2-i]  == player for i in rows))
 }
 
 void apply_move(struct board *b, int move, int player) {
@@ -142,6 +145,19 @@ void show_moves(int moves[]) {
  * stuff related to which moves to take, independent of board data structure
  */
 
+int random_move(int moves[]) {
+    int n = 0;
+    int i;
+    for (i = 0; i < 81; i++) {
+        n += moves[i];
+    }
+    n = rand() % n;
+    for (i = 0; i < 81; i++) {
+        if (moves[i] && n-- == 0) return i;
+    }
+    return -1;
+}
+
 int is_winning_move(struct board *b, int move, int player) {
     return 0;
 	//~""" get move that leads to immediate victory, if any, or None
@@ -157,46 +173,50 @@ int is_non_losing_move(struct board *b, int move, int player) {
 	        //~if not winning_move(nb, get_moves(nb, m), -player)]
 }
 
+/*
+ * perform random moves until the game is over,
+ * return winning player and number of moves
+ */
 int random_play(struct board *b, int move, int player) {
-    return 0;
-	//~""" perform random moves until the game is over,
-	//~return winning player and number of moves
-	//~"""
-	//~board = apply_move(board, move, player)
-	//~for i in itertools.count():
-		//~if winning(board, player):
-			//~return player, i
-		//~moves = get_moves(board, move)
-		//~if not moves:
-			//~return winning_draw(board, player), i
-		//~player = -player
-		//~move = random.choice(moves)
-		//~board = apply_move(board, move, player)
+    int moves[81];
+	apply_move(b, move, player);
+    while (1) {
+        if (winning(b->meta, player)) {
+            return player;
+        }
+        get_moves(b, move, moves);
+        if (! contains(moves, 81, 1)) {
+            return winning_draw(b->meta, player);
+        }
+        
+        player = -player;
+        move = random_move(moves);
+        apply_move(b, move, player);
+    }
 }
 
-void evaluate_moves(struct board *b, int moves[], int player, int scores[], int timeout) {
-	//~""" perform random plays for random moves until time is up,
-	//~"""
-	//~scores = {move: 0 for move in moves}
-	//~start = time.time()
-	//~total = 0
-	//~for i in itertools.count():
-		//~if time.time() - start > timeout:
-			//~break
-		//~move = random.choice(moves)
-		//~r, c = random_play(board, move, player)
-		//~scores[move] += player * r
-		//~total += c
-	//~debug(i, total, scores)
-	//~return scores
+void evaluate_moves(struct board *b, int moves[], int player, int scores[]) {
+    int i;
+    struct board b2;
+    for (i = 0; i < 1000; i++) {
+        copy(b, &b2);
+        int move = random_move(moves);
+        int win = random_play(&b2, move, player);
+        scores[move] += player * win;
+    }
+    show_moves(scores);
 }
 
 int best_move_random_plays(struct board *b, int moves[], int player) {
-    return 0;
-	//~""" select move with best win/loss ratio
-	//~"""
-	//~scores = evaluate_moves(board, moves, player, TIMEOUT)
-	//~return max((scores[m], m) for m in moves)
+    int scores[81], i;
+    for (i = 0; i < 81; i++) scores[i] = 0;
+	evaluate_moves(b, moves, player, scores);
+    int best = 0;
+    for (i = 1; i < 81; i++) {
+        if (scores[i] > scores[best]) best = i;
+    }
+    printf("move %d; score %d\n", best, scores[best]);
+    return best;
 }
 
 int best_move(struct board *b, int moves[], int player) {
@@ -219,37 +239,26 @@ int best_move(struct board *b, int moves[], int player) {
  */
 
 int play_game() {
-    return 0;
-	//~board = init_board()
-	//~player, move = random.choice([OWN, OPP]), (-1, -1)
-	//~for i in itertools.count():
-		//~moves = get_moves(board, move)
-		//~if not moves:
-			//~return winning_draw(board, player)
-
-		//~score, move = best_move(board, moves, player)
-		//~
-		//~debug("{} move: {}, score: {}".format(i, move, score))
-		//~board = apply_move(board, move, player)
-
-		//~show_grid(board)
-		//~
-		//~if winning(board, player):
-			//~return player
-		//~player = -player
-}
-
-int random_move(int moves[]) {
-    int n = 0;
-    int i;
-    for (i = 0; i < 81; i++) {
-        n += moves[i];
+	struct board *b = init_board();
+    int player = rand() % 2 ? 1 : -1;
+    int move = -1;
+    int moves[81];
+    int turn = 0;
+    while (1) {
+        get_moves(b, move, moves);
+        if (! contains(moves, 81, 1)) {
+            return winning_draw(b->meta, player);
+        }
+        move = best_move(b, moves, player);
+		apply_move(b, move, player);
+		show_grid(b);
+        
+		if (winning(b->meta, player)) {
+			return player;
+        }
+		player = -player;
+        turn++;
     }
-    n = rand() % n;
-    for (i = 0; i < 81; i++) {
-        if (moves[i] && n-- == 0) return i;
-    }
-    return -1;
 }
 
 void test_board() {
@@ -284,7 +293,9 @@ void test_board() {
 
 int main() {
     
-    test_board();
+    //~test_board();
+    
+    play_game();
     
 	//~seed = random.randrange(1000000)
 	//~random.seed(seed)
